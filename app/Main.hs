6fa90f44046f5fbeaaf805845b3c3ba4df46fcd5
Jack Martin <jpm61704@gmail.com>
2017-12-11 10:06:34 -0800

    final commit before submission

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,7 @@ import           CipherData
 import           CWord
 import           Decipher
 import           Dictionary
+import qualified Encrypt            as E
 import           Percentage
 import           System.Environment
 import           System.Random
@@ -16,16 +17,36 @@ parseArgs = do
   let rep = percentage $ read $ strs !! 2
   return (pop, mut, rep)
 
+getTextToEncrypt :: IO CText
+getTextToEncrypt = do
+  putStrLn "Please enter the text to be encrypted. (Only letters and spaces)"
+  x <- getLine
+  case fromString x of
+    (Just str) -> return str
+    Nothing    -> getTextToEncrypt
+
+getKey :: IO Key
+getKey = do
+  putStrLn "Please enter the key to encrypt with (Only letters)"
+  x <- getLine
+  return $ keyFromStr x
+
+
 main :: IO ()
 main = do
   dict <- stdDict
   gen <- newStdGen
   e_dict <- commonDict
+
+  text <- getTextToEncrypt
+  key <- getKey
+  let encrypted_text = E.encode key text
+
   putStrLn "beggining evolution"
   args@(p,m,r) <- parseArgs
   putStrLn $ "\tpop_size:\t" ++ show p
   putStrLn $ "\tmutate_rate:\t" ++ show m
   putStrLn $ "\treproduction:\t" ++ show r
-  key <- decipher gen dict e_dict 5 args longText2
+  key <- decipher gen dict e_dict (keyLength key) args encrypted_text
   print key
   return ()
--- src/Decipher.hs
+++ src/Decipher.hs
@@ -14,16 +14,26 @@ import           Percentage
 import           System.IO
 import           System.Random
 
+
+-- * Decoding and Keys
+
 newtype Key = Key (V.Vector Letter) deriving (Eq)
 
 instance Show Key where
   show (Key k) = concat $ fmap (show) k
+
+keyLength :: Key -> Int
+keyLength (Key k) = V.length k
+
 
 keyFromList :: [Letter] -> Key
 keyFromList = Key . V.fromList
 
 keyToCText :: Key -> CText
 keyToCText (Key k) = fmap Letter (V.toList k)
+
+keyFromStr :: String -> Key
+keyFromStr = keyFromList . (fmap (fromJust . convertLetter))
 
 keyToIntList :: Key -> [Int]
 keyToIntList (Key xs) = map fromEnum (V.toList xs)
@@ -61,21 +71,11 @@ decode k t = subText t additions
   where additions = case keyToCText k of
                       k' -> mconcat $ repeat k'
 
------------------------------------------------------
---------------- evolutionary solution ---------------
------------------------------------------------------
 
-{-
-decipher :: StdGen -> Dictionary -> Int -> EncryptedText -> (CText, Key)
-decipher gen d key_len text = (decode key text, key)
-  where key = evalState (decipher' d text (randomInitialPopulation 100 key_len)) gen
--}
-hoistState :: Monad m => State s a -> StateT s m a
-hoistState = StateT . (return .) . runState
+-- * Evolutionary Decoding
 
-hoistStateT :: Monad m => StateT s (State t) a -> StateT s (StateT t m) a
-hoistStateT = undefined
 
+-- | This is the main decipher function.
 decipher :: StdGen
          -> Dictionary
          -> Dictionary
@@ -85,12 +85,16 @@ decipher :: StdGen
          -> IO Key
 decipher gen cutoff_dict evolve_dict key_len (pop_size, mutate_rate, percent_parents) text = do
   let xs = evalState (randomInitialPopulation pop_size key_len) gen
-  x <- evalStateT (evalStateT (evolve (mutate_rate, percent_parents) cutoff_dict evolve_dict text) xs) gen
+  x <- evalStateT (evalStateT (evolve2 (mutate_rate, percent_parents) cutoff_dict evolve_dict text) xs) gen
   return x
 
+{- |evolves the population until a solution is found
+    a solution is found when a key deciphers a messassge
+    in which all  words are contained in a given dictionary
+-}
 evolve :: (Percentage, Percentage)
-       -> Dictionary
-       -> Dictionary
+       -> Dictionary -- ^ the complete dictionary to evaluate solution keys
+       -> Dictionary -- ^ the partial dictionary to decode with
        -> EncryptedText
        -> StateT (V.Vector Key) (StateT StdGen IO) Key
 evolve config cutoff_dict evolve_dict text = do
@@ -123,12 +127,31 @@ evolve2 config cutoff_dict evolve_dict text = do
     Nothing     -> evolve config cutoff_dict evolve_dict text
 
 
-printKeys :: StateT (V.Vector Key) (StateT StdGen IO) ()
-printKeys = do
-  ks <- get
-  lift $ lift $ putStrLn $ "\tsample: " ++ (show (ks V.! 0)) ++ "\t" ++ (show (ks V.! 1)) ++ "\t" ++ (show (ks V.! 2))
-  return ()
+-- the threshold is the double size to be larger then in order to qualify as a candidate solution
+checkSolution :: V.Vector Key -> V.Vector Double -> (Maybe Key)
+checkSolution keys ranks = do
+  case V.findIndex (== 1) ranks of
+    (Just sol) -> keys V.!? sol
+    Nothing    -> Nothing
 
+-- | generates a n random keys of length len
+randomInitialPopulation :: Int -> Int -> State StdGen (V.Vector Key)
+randomInitialPopulation n len = do
+  let bounds = (keyFromList (replicate len A), keyFromList (replicate len Z))
+  x  <- state $ randomR bounds
+  xs <- randomInitialPopulation (n - 1) len
+  return $ if n > 1 then V.cons x xs else V.singleton x
+
+
+{-
+evaluateKey' :: Dictionary -> EncryptedText -> Key -> Double
+evaluateKey' d e k = dist
+  where text = decode k e
+        wrds = cwords text
+        dist    = product $ map (maximum . wordMatchDistances d) wrds
+-}
+
+-- * Reproduction
 
 nextGeneration :: (Percentage, Percentage)
                -> Dictionary
@@ -158,42 +181,6 @@ breed n = do
   put $ mappend kid kids
   return ()
 
--- the threshold is the double size to be larger then in order to qualify as a candidate solution
-checkSolution :: V.Vector Key -> V.Vector Double -> (Maybe Key)
-checkSolution keys ranks = do
-  case V.findIndex (== 1) ranks of
-    (Just sol) -> keys V.!? sol
-    Nothing    -> Nothing
-
-
-mutate :: Percentage -> StateT (V.Vector Key) (State StdGen) ()
-mutate p = do
-  ks <- get
-  lift $ forM ks ((chanceMutateKey p))
-  return ()
-
-chanceMutateKey :: Percentage -> Key -> State StdGen Key
-chanceMutateKey p k = do
-  t <- state $ random
-  if t < p
-    then mutateKey k
-    else return k
-
-mutateKey :: Key -> State StdGen Key
-mutateKey (Key letters) = do
-  i <- state $ randomR (0, length letters - 1)
-  new_letter <- state random
-  let new_vec = V.update letters (V.singleton (i, new_letter))
-  return $ Key new_vec
-
-
-
-randomInitialPopulation :: Int -> Int -> State StdGen (V.Vector Key)
-randomInitialPopulation n len = do
-  let bounds = (keyFromList (replicate len A), keyFromList (replicate len Z))
-  x  <- state $ randomR bounds
-  xs <- randomInitialPopulation (n - 1) len
-  return $ if n > 1 then V.cons x xs else V.singleton x
 
 twoPointCrossOver :: Key -> Key -> State StdGen (V.Vector Key)
 twoPointCrossOver (Key x) (Key y) = do
@@ -220,25 +207,37 @@ crossOver (Key x) (Key y) = do
       k2       = V.singleton $ Key (mappend y1 x2)
   return $ mappend k1 k2
 
-selectTopXPercent :: Percentage -> (V.Vector Double) -> V.Vector Key -> V.Vector Key
-selectTopXPercent percent ranks keys
-  | length ranks == length keys = V.ifilter (\i x -> getranki i >= cutoff) keys
-  | otherwise = error "pop and ranks not same length"
-  where getranki = \i -> fromJust $ ranks V.!? i
-        cutoff = (sort $ V.toList ranks) !! (length ranks - (floor (percentile percent (fromIntegral (length ranks)))))
+
+-- * Mutation
+
+mutate :: Percentage -> StateT (V.Vector Key) (State StdGen) ()
+mutate p = do
+  ks <- get
+  lift $ forM ks ((chanceMutateKey p))
+  return ()
+
+chanceMutateKey :: Percentage -> Key -> State StdGen Key
+chanceMutateKey p k = do
+  t <- state $ random
+  if t < p
+    then mutateKey k
+    else return k
+
+mutateKey :: Key -> State StdGen Key
+mutateKey (Key letters) = do
+  i <- state $ randomR (0, length letters - 1)
+  new_letter <- state random
+  let new_vec = V.update letters (V.singleton (i, new_letter))
+  return $ Key new_vec
+
+
+-- * Selection
 
 rankPopulation :: Dictionary -> EncryptedText -> V.Vector Key -> (Double, V.Vector Double)
 rankPopulation d e ks = (avg, ranks)
   where ranks = fmap (evaluateKey d e) ks
         avg   = (sum ranks) / (fromIntegral (length ranks))
 
-{-
-evaluateKey' :: Dictionary -> EncryptedText -> Key -> Double
-evaluateKey' d e k = dist
-  where text = decode k e
-        wrds = cwords text
-        dist    = product $ map (maximum . wordMatchDistances d) wrds
--}
 
 evaluateKey :: Dictionary -> EncryptedText -> Key -> Double
 evaluateKey d e k = dist
@@ -256,3 +255,26 @@ matchDistance x y
   | otherwise = (fromIntegral matches) / (fromIntegral len)
   where matches = foldr (\x acc -> if x then acc + 1 else acc) 0 (zipWith (==) x y)
         len = length x
+
+
+selectTopXPercent :: Percentage -> (V.Vector Double) -> V.Vector Key -> V.Vector Key
+selectTopXPercent percent ranks keys
+  | length ranks == length keys = V.ifilter (\i x -> getranki i >= cutoff) keys
+  | otherwise = error "pop and ranks not same length"
+  where getranki = \i -> fromJust $ ranks V.!? i
+        cutoff = (sort $ V.toList ranks) !! (length ranks - (floor (percentile percent (fromIntegral (length ranks)))))
+
+-- * Misc
+
+printKeys :: StateT (V.Vector Key) (StateT StdGen IO) ()
+printKeys = do
+  ks <- get
+  lift $ lift $ putStrLn $ "\tsample: " ++ (show (ks V.! 0)) ++ "\t" ++ (show (ks V.! 1)) ++ "\t" ++ (show (ks V.! 2))
+  return ()
+
+
+hoistState :: Monad m => State s a -> StateT s m a
+hoistState = StateT . (return .) . runState
+
+hoistStateT :: Monad m => StateT s (State t) a -> StateT s (StateT t m) a
+hoistStateT = undefined
